0b5144e65f76dc1a6f6cc675cd1f1ce9bb7a1459
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-02-10 14:13:52 -0700

    Added code comments.

--- src/hashtable.py
+++ src/hashtable.py
@@ -7,15 +7,16 @@ class LinkedPair:
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -24,7 +25,6 @@ class HashTable:
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return self._hash_djb2(key)
-
 
     def _hash_djb2(self, key):
         '''
@@ -37,14 +37,12 @@ class HashTable:
             hash = ((hash << 5) + hash) + ord(x)
         return hash & 0xFFFFFFFF
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -54,21 +52,30 @@ class HashTable:
 
         Fill this in.
         '''
+
+        # Get the hashed key
         hash_key = self._hash_mod(key)
+
+        # check if there is a LL in the array at the hash key index.
         if self.storage[hash_key] == None:
+            # if no LL make one
             self.storage[hash_key] = LinkedPair(key, value)
         else:
+            # Get the LL
             node = self.storage[hash_key]
 
+            # check to see if key at the node is the same
             while node.next is not None and node.key != key:
+                # if not the same go to next node
                 node = node.next
 
+            # check is the key is the same.
             if node.key == key:
+                # set value = to the new value. overwrite the node
                 node.value = value
             else:
+                # create a new node for the key because it wasn't found
                 node.next = LinkedPair(key, value)
-
-
 
     def remove(self, key):
         '''
@@ -78,20 +85,27 @@ class HashTable:
 
         Fill this in.
         '''
+        # get hash index
         hash_key = self._hash_mod(key)
+
+        # get the LL at the given hash index
         node = self.storage[hash_key]
+
+        # check if the node key is the same
         if node.key == key:
+            # remove node by setting the LL head to the next node
             self.storage[hash_key] = node.next
         else:
+            # search the LL for the key
             while node.next is not None and node.next.key != key:
                 node = node.next
 
+            # check if node.next key is the same.
             if node.next.key == key:
+                # remove next node
                 node.next = node.next.next
             else:
                 print("The key was not found.")
-
-
 
     def retrieve(self, key):
         '''
@@ -101,16 +115,21 @@ class HashTable:
 
         Fill this in.
         '''
+        # get the hash index
         hash_key = self._hash_mod(key)
+
+        # get the LL at the index
         node = self.storage[hash_key]
+
+        # search the LL for the key
         while node is not None and node.next is not None and node.key != key:
             node = node.next
 
+        # if the node key is the same return the value
         if node is not None and node.key == key:
             return node.value
         else:
             return None
-
 
     def resize(self):
         '''
@@ -119,15 +138,21 @@ class HashTable:
 
         Fill this in.
         '''
+        # copy the old storage
         old_storage = self.storage
+
+        # double capacity
         self.capacity = self.capacity * 2
+
+        # create new storage
         self.storage = [None] * self.capacity
+
+        # loop through old storage inserting items one by one in to new storage
         for i in range(len(old_storage)):
             node = old_storage[i]
             while node is not None:
                 self.insert(node.key, node.value)
                 node = node.next
-
 
 
 if __name__ == "__main__":
